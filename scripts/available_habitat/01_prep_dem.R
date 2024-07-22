7ee6969bd21d75f032b94d35d2522653ae065ac1
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-07-22 11:53:56 -0600

    cleaned up 01_prep_dem script

--- scripts/available_habitat/01_prep_dem.R
+++ scripts/available_habitat/01_prep_dem.R
@@ -40,29 +40,6 @@ filled_mosaic_crs32611 = terra::project(filled_mosaic, "EPSG:32611")
 filled_mosaic_raster = raster(filled_mosaic_crs32611)
 
 # save the final dem
-writeRaster(filled_mosaic_raster, "C:/Workspace/gis/snake_river_10m_ned_dem.tif", overwrite = TRUE)
+writeRaster(filled_mosaic_raster, here("data/spatial/snake_river_10m_ned_dem.tif"), overwrite = TRUE)
 
 ### END SCRIPT
-
-# Fill in NA values using terra::focal
-# mosaic_terra <- rast(mosaic)  # Convert raster to terra object
-# filled_mosaic <- focal(mosaic_terra, w = matrix(1, 3, 3), fun = mean, na.rm = TRUE, NAonly = TRUE)
-# 
-# # Transform to CRS 32611
-# filled_mosaic <- project(filled_mosaic, "EPSG:32611")
-# 
-# # Convert back to raster if needed
-# filled_mosaic_raster <- raster(filled_mosaic)
-# 
-# # Save the filled and transformed DEM
-# writeRaster(filled_mosaic_raster, "filled_mosaic_32611.tif", overwrite = TRUE)
-# 
-# # THIS ROUTE PROVIDED PROMISE; MOVE TO ANOTHER SCRIPT AND MAY NEED TO CONSIDER TRANSFORMING AND FILLING HOLES
-# 
-# # load dem tiles
-# dem_files = list.files("C:/Workspace/gis/10m_NED_DEMS", pattern = "*.tif", full.names = T)
-# dem_list = lapply(dem_files, raster)
-# m = do.call(merge, x)
-# 
-# # Save the final DEM
-# writeRaster(m, "C:/Workspace/gis/snake_river_10m_ned_dem.tif", overwrite = TRUE)
