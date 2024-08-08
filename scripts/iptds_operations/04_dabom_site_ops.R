67932de94ccb2a0a2fa6f1546680f7798cb89f09
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-08-08 10:35:06 -0600

    summarized "operations" of all sites used in dabom by species, site, and year, draft

--- scripts/iptds_operations/04_dabom_site_ops.R
+++ scripts/iptds_operations/04_dabom_site_ops.R
@@ -15,6 +15,7 @@ library(tidyverse)
 library(here)
 library(sf)
 library(readxl)
+library(writexl)
 
 # load configuration files
 load("C:/Git/SnakeRiverFishStatus/data/configuration_files/site_config_LGR_20240304.rda") ; rm(flowlines, configuration, node_paths, parent_child, pc_nodes)
@@ -26,7 +27,7 @@ dabom_site_pops = sites_sf %>%
   filter(str_extract(rkm, "^[0-9]{3}") == "522" & as.numeric(str_extract(rkm, "(?<=\\.)[0-9]{3}")) >= 100) %>%
   # remove GOA, LGR, and GRS
   filter(!site_code %in% c("GOA", "LGR", "GRS")) %>%
-  select(site_code, geometry) %>%
+  select(site_code, site_type, geometry) %>%
   st_transform(crs = 4326) %>%
   # join steelhead, sp/sum chinook, and (made up) coho populations
   st_join(sth_pop %>% 
@@ -55,6 +56,11 @@ dabom_site_pops = sites_sf %>%
     site_code %in% c("SC1", "SC2") & species == "sthd" ~ "CRSFC-s",
     site_code == "USI"             & species == "sthd" ~ "SREFS-s",
     TRUE ~ trt_pop
+  )) %>%
+  # STL and STR are MRR sites
+  mutate(site_type = case_when(
+    site_code %in% c("STR", "STL") ~ "MRR",
+    TRUE ~ site_type
   )) %>%
   st_drop_geometry() ; rm(spsm_pop, sth_pop)
 
@@ -99,15 +105,86 @@ dabom_int_ops = ptagis_ops %>%
   # add populations; use right_join to remove sites not in dabom_site_pops
   right_join(dabom_site_pops %>%
                # exclude MRR sites for now
-               filter(nchar(site_code) == 3),
+               filter(site_type == "INT") %>%
+               select(species, site_code, trt_pop),
              by = c("species", "site_code")) %>%
   # for coho, remove any records prior to 2023
   filter(!(species == "coho" & spawn_year < 2023)) %>%
   arrange(site_code, spawn_year, species) %>%
   mutate(use_for_pop_abundance = NA)
 
-# save dabom site operations summary
-# save(site_ops,
-#      file = here("output/iptds_operations/site_operations_summary.rda"))
+# read in complete tag histories since SY2010
+tags_by_site = list.files(path = "C:/Git/SnakeRiverFishStatus/data/complete_tag_histories/",
+                          pattern = "\\.csv$",
+                          full.names = T) %>%
+  setNames(nm = .) %>%
+  map_df(~read_csv(.x, show_col_types = F), .id = "file_name") %>%
+  # add species and spawn year
+  mutate(file_name = str_replace(file_name, ".*/", ""), 
+         species = str_extract(file_name, "(?<=_)[^_]+"),       
+         spawn_year = str_extract(file_name, "SY[0-9]{4}")) %>%
+  select(-file_name) %>%
+  # summarize number of unique tags observed by site
+  select(species,
+         spawn_year,
+         site_code = `Event Site Code Value`,
+         tag_code = `Tag Code`) %>%
+  filter(site_code %in% (dabom_site_pops %>%
+                           #filter(site_type == "MRR") %>%
+                           pull(site_code) %>%
+                           unique())) %>%
+  distinct() %>%
+  mutate(spawn_year = as.numeric(gsub("SY", "", spawn_year))) %>%
+  mutate(species = case_when(
+    species == "Chinook" ~ "chnk",
+    species == "Steelhead" ~ "sthd",
+    species == "Coho" ~ "coho"
+  )) %>%
+  group_by(species,
+           spawn_year,
+           site_code) %>%
+  summarise(n_tags = n(), .groups = "drop")
+
+# unique species and spawn_year combinations in dabom_int_ops
+spc_sy = dabom_int_ops %>%
+  select(species, spawn_year) %>%
+  distinct()
+
+# summarize operations for mrr sites used in dabom
+dabom_mrr_ops = dabom_site_pops %>%
+  select(site_code, site_type) %>%
+  filter(site_type == "MRR") %>%
+  select(-site_type) %>%
+  distinct() %>%
+  # expand based on spc_yr
+  crossing(spc_sy) %>%
+  # add the number of tags observed for a given species and spawn year (if any)
+  left_join(tags_by_site, 
+            by = c("species", "site_code", "spawn_year")) %>%
+  mutate(n_tags = replace_na(n_tags, 0)) %>%
+  # set auto_operational to TRUE if tags were observed
+  mutate(auto_operational = n_tags > 0) %>%
+  mutate(user_operational = ifelse(auto_operational, TRUE, NA)) %>%
+  select(-n_tags)
+
+# merge the int and mrr operations
+dabom_ops = bind_rows(dabom_int_ops, dabom_mrr_ops) %>%
+  left_join(tags_by_site, 
+            by = c("species", "site_code", "spawn_year")) %>%
+  mutate(n_tags = replace_na(n_tags, 0)) %>%
+  select(species, 
+         trt_pop, 
+         site_code, 
+         spawn_year, 
+         p_days_ptagis_run,
+         p_days_ptagis_yr,
+         p_vtt,
+         n_transceiver,
+         n_tags,
+         everything()) %>%
+  arrange(site_code, species, trt_pop, spawn_year)
+
+# write dabom site operations summary to excel file
+#write_xlsx(dabom_ops, path = paste0(here("output/iptds_operations/dabom_site_operations_"), Sys.Date(), ".xlsx"))
 
 ### END SCRIPT
