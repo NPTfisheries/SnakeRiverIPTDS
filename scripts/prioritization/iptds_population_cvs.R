12e32e61ffad9a50372cbcb7e4895a42eaaf660b
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-12-11 08:46:03 -0700

    minor updates to cvs script to update prioritization report

--- scripts/prioritization/iptds_population_cvs.R
+++ scripts/prioritization/iptds_population_cvs.R
@@ -4,7 +4,7 @@
 #            for Snake River populations.
 # 
 # Created: April 4, 2024
-#   Last Modified:
+#   Last Modified: December 11, 2024
 # 
 # Notes:
 
@@ -17,16 +17,16 @@ library(tidyverse)
 library(here)
 
 # compile data
-sthd_df = read_excel("C:/Git/SnakeRiverFishStatus/output/syntheses/LGR_Steelhead_all_summaries_2024-06-20.xlsx",
+sthd_df = read_excel("C:/Git/SnakeRiverFishStatus/output/syntheses/LGR_Steelhead_all_summaries_2024-12-09.xlsx",
                      sheet = "Pop_Tot_Esc",
                      progress = F)
 
-chnk_df = read_excel("C:/Git/SnakeRiverFishStatus/output/syntheses/LGR_Chinook_all_summaries_2024-06-20.xlsx",
+chnk_df = read_excel("C:/Git/SnakeRiverFishStatus/output/syntheses/LGR_Chinook_all_summaries_2024-12-09.xlsx",
                      sheet = "Pop_Tot_Esc",
                      progress = F)
   
 # steelhead
-sthd_pops = data.frame(TRT_POPID = 
+sthd_pops = data.frame(popid = 
                          c("SRUMA-s", "SREFS-s", "SRPAH-s", "SRLEM-s", "SRNFS-s", "SRPAN-s", "SRCHA-s", # Salmon River
                            "MFUMA-s", "MFBIG-s", "SFMAI-s", "SFSEC-s", "SRLSR-s", 
                            "CRSEL-s", "CRLOC-s", "CRSFC-s", "CRLOL-s", "CRNFC-s", "CRLMA-s",            # Clearwater River
@@ -36,9 +36,7 @@ sthd_pops = data.frame(TRT_POPID =
                            "SNASO-s", "SNTUC-s"))                                                       # Lower Snake
 
 sthd_cvs = sthd_df %>%
-  select(spawn_yr, TRT_POPID, valid_est, cv) %>%
-  filter(valid_est == 1) %>%
-  select(-valid_est) %>%
+  select(spawn_yr, popid, cv) %>%
   mutate(cv = cv * 100) %>%
   pivot_wider(names_from = spawn_yr,
               values_from = cv) %>%
@@ -48,10 +46,10 @@ sthd_cvs = sthd_df %>%
   # fill in missing populations
   right_join(sthd_pops) %>%
   # arrange to match sthd_pops
-  arrange(match(TRT_POPID, sthd_pops$TRT_POPID))
+  arrange(match(popid, sthd_pops$popid))
 
 # chinook
-chnk_pops = data.frame(TRT_POPID = 
+chnk_pops = data.frame(popid = 
                          c("SRUMA", "SRVAL", "SRLMA", "SRYFS", "SREFS", "SRPAH", "SRLEM", "SRNFS", "SRPAN", # Upper Salmon River
                            "MFBEA", "MFMAR", "MFSUL", "MFUMA", "MFLMA", "MFLOO", "MFCAM", "MFBIG", "SRCHA", # Middle Fork Salmon
                            "SFSMA", "SFEFS", "SFSEC", "SRLSR",                                              # South Fork Salmon
@@ -59,9 +57,7 @@ chnk_pops = data.frame(TRT_POPID =
                            "SNASO", "SNTUC"))                                                               # Lower Snake
 
 chnk_cvs = chnk_df %>%
-  select(spawn_yr, TRT_POPID, valid_est, cv) %>%
-  filter(valid_est == 1) %>%
-  select(-valid_est) %>%
+  select(spawn_yr, popid, cv) %>%
   mutate(cv = cv * 100) %>%
   pivot_wider(names_from = spawn_yr,
               values_from = cv) %>%
@@ -71,7 +67,7 @@ chnk_cvs = chnk_df %>%
   # fill in missing populations
   right_join(chnk_pops) %>%
   # arrange to match sthd_pops
-  arrange(match(TRT_POPID, chnk_pops$TRT_POPID))                      
+  arrange(match(popid, chnk_pops$popid))                      
 
 # write tables to .csvs
 write_csv(sthd_cvs,
