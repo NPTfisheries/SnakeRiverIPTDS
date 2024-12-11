db470a15057f24c857a8d00417ba248ac8084b84
Mike Ackerman <dirtpathwanderer@gmail.com>
2024-12-11 16:35:33 -0700

    cleaning up iptds website

--- scripts/leaflet/sr_ipdts_leaflet_w_recommendations.R
+++ scripts/leaflet/sr_ipdts_leaflet_w_recommendations.R
@@ -5,7 +5,7 @@
 #   upgrades, etc. to aid in planning and discussions.
 # 
 # Created: April 11, 2024
-#   Last Modified: September 27, 2024
+#   Last Modified: December 11, 2024
 # 
 # Notes: Much of this is based on a previous script iptds_planning.R from RK. This script produces the default leaflet
 #   with Snake River prioritization recommendations.
@@ -22,20 +22,22 @@ library(leaflet)
 library(htmlwidgets)
 
 # -----------------------
-# COMPILE DATA
+# compile data
 
 # current iptds data
-iptds_cur = read_excel(here("data/prioritization/Snake River IPTDS Prioritization 20240417.xlsx"),
-                       sheet = "SR_IPTDS_Sites") %>%
+iptds_sf = read_excel(here("data/prioritization/Maintained Snake River IPTDS Metadata 20241211.xlsx"),
+                      sheet = "SR_IPTDS_Sites") %>%
   st_as_sf(coords = c("longitude", "latitude"),
            crs = 4326)
 
 # iptds recommendations
-iptds_rec = read_excel(here("data/prioritization/iptds_site_recommendations_20240904.xlsx"),
-                       sheet = "Sheet1") %>%
+recommendations_sf = read_excel(here("data/prioritization/iptds_site_recommendations_20241211.xlsx"),
+                                sheet = "Sheet1") %>%
   filter(!site_code %in% c("EFS (Proposed)",
                            "WB1 (Proposed)",
-                           "SLT (Proposed)")) %>%
+                           "SLT (Proposed)",
+                           # based on e-mail from wdfw 11/27/2024
+                           "ACM")) %>%
   st_as_sf(coords = c("longitude", "latitude"),
            crs = 4326)
   
@@ -43,7 +45,6 @@ iptds_rec = read_excel(here("data/prioritization/iptds_site_recommendations_2024
 load(here("data/spatial/SR_pops.rda")) ; rm(fall_pop)
 sthd_pops = sth_pop %>%
   select(TRT_POPID, POP_NAME, MPG) ; rm(sth_pop)
-
 chnk_pops = spsm_pop %>%
   select(TRT_POPID, POP_NAME, MPG) ; rm(spsm_pop)
 
@@ -63,25 +64,28 @@ chnk_spawn = readRDS(here("data/spatial/spsm_spwn_areas.rds")) %>%
   st_transform("EPSG:4326")
 
 # -----------------------
-# SET SOME COLORS
+# set some colors
 sthd_mpg_col = colorFactor(palette = "Dark2", domain = sthd_pops$MPG)
 sthd_spawn_col = colorFactor(palette = c("skyblue", "navy"), domain = sthd_spawn$TYPE, reverse = TRUE)
 chnk_mpg_col = colorFactor(palette = "Set1", domain = chnk_pops$MPG)
 chnk_spawn_col = colorFactor(palette = c("springgreen", "darkgreen"), domain = chnk_spawn$TYPE, reverse = TRUE)
-rec_col = colorFactor(palette = c("black", "red3", "royalblue4", "goldenrod3"), 
-                      domain = iptds_rec$recommendation,
-                      levels = c("Continue Funding", "Proposed New Site", "Candidate for O&M Project", "Decommission, Remove, or Transfer"))
-cur_col = colorFactor(palette = c("gray50", "black"), domain = iptds_cur$integrated_om_site)
+
+recommendation_col = colorFactor(palette = c("black", "darkorchid2", "deepskyblue2", "springgreen3"), 
+                                 domain = iptds_rec$recommendation,
+                                 levels = c("Continue Funding", "Proposed New Site", "Candidate for O&M Project", "Decommission, Remove, or Transfer"))
+
+om_col = colorFactor(palette = c("gray50", "black"), domain = iptds_sf$integrated_om_site)
+
 priority_col = colorFactor(c("red", "orange", "yellow"), 
-                           domain = iptds_rec$action_priority, 
+                           domain = recommendations_sf$action_priority, 
                            levels = c("HIGH", "MED", "LOW"),
                            na.color = "transparent")
 
 # -----------------------
-# BUILD LEAFLET
+# build leaflet
 base = leaflet() %>%
   # base maps
-  setView(lng = -115.5660, lat = 45.4000, zoom = 7.5) %>%
+  setView(lng = -116, lat = 45.35, zoom = 7.5) %>%
   addProviderTiles(providers$Esri.WorldTopoMap) %>%
   addPolylines(data = streams, color = "blue", weight = 1)
 
@@ -135,77 +139,88 @@ sr_iptds_leaflet = base %>%
               opacity = 1,
               label = ~paste0(POP_NAME, ": ", MSA_NAME, ", ", TYPE)) %>%
   # add current iptds
-  addCircles(data = iptds_cur,
-             group = "Current IPTDS Sites",
-             label = ~site_code,
-             color = ~cur_col(integrated_om_site),
-             opacity = 1,
-             weight = 10,
-             popup = paste("<b>Site Code:</b>", iptds_cur$site_code, "</br>",
-                           "<b>Site Name:</b>", iptds_cur$site_name, "</br>",
-                           "<b>Stream:</b>", iptds_cur$stream, "</br>",
-                           "<b>Node Count:</b>", iptds_cur$node_count, "</br>",
-                           "<b>Antenna Count:</b>", iptds_cur$antenna_count, "</br>",
-                           "<b>PTAGIS Active:</b>", iptds_cur$ptagis_active, "</br>",
-                           "<b>Detection Probabilities:</b>", iptds_cur$detection_prob, "</br>",
-                           "<b>Biomark Integrated O&M Site:</b>", iptds_cur$integrated_om_site, "</br>",
-                           "<b>Status and Trends:</b>", iptds_cur$adult_status_trends, "</br>",
-                           "<b>Current Funding:</b>", iptds_cur$current_funding, "</br>",
-                           "<b>BPA Funding Type:</b>", iptds_cur$bpa_funding, "</br>",
-                           "<b>O&M Agency:</b>", iptds_cur$om_agency, "</br>",
-                           "<b>Site Description:</b>", iptds_cur$site_description, "</br>")) %>%
-  # add iptds recommendations
-  addCircles(data = iptds_rec,
-             group = "IPTDS Recommendations",
-             label = ~site_code,
-             color = ~rec_col(recommendation),
-             opacity = 1,
-             weight = 10,
-             popup = paste("<b>Site Code:</b>", iptds_rec$site_code, "</br>",
-                           "<b>Priority:</b>", iptds_rec$action_priority, "</br>",
-                           "<b>Notes:</b>", iptds_rec$notes, "</br>")) %>%
-  # dabom mrr sites
-  addCircles(data = mrr_sites,
-             group = "DABOM MRR Sites",
-             label = ~site_code,
-             color = "hotpink",
-             opacity = 1,
-             weight = 5,
-             popup = paste("<b>Site Code:</b>", mrr_sites$site_code, "</br>",
-                           "<b>Site Name:</b>", mrr_sites$site_name, "</br>",
-                           "<b>DABOM Node:</b>", mrr_sites$node, "</br>")) %>%
-  # color by status and trends tier
-  addCircleMarkers(data = iptds_rec,
-                   group = "Action Priority",
-                   color = ~priority_col(action_priority)) %>%
-  # control layers
-  addLayersControl(baseGroups = c("Steelhead Populations",
-                                  "Steelhead Spawning Areas",
-                                  "Sp/Sum Chinook Populations",
-                                  "Sp/Sum Chinook Spawning Areas"),
-                   overlayGroups = c("IPTDS Recommendations",
-                                     "Current IPTDS Sites",
-                                     "DABOM MRR Sites",
-                                     "Action Priority"),
-                   options = layersControlOptions(collapsed = FALSE)) %>%
-  # add legends
-  # addLegend(data = sthd_spawn,       # consider moving this to polygon labels, instead
-  #           position = "bottomleft",
-  #           pal = sthd_spawn_col,
-  #           values = ~TYPE,
-  #           title = "Spawning Area Type",
-  #           group = "Steelhead Spawning Areas",
-  #           opacity = 0.5) %>%
-  # addLegend(data = chnk_spawn,       # consider moving this to polygon labels, instead
-  #           position = "bottomleft",
-  #           pal = chnk_spawn_col,
-  #           values = ~TYPE,
-  #           title = "Spawning Area Type",
-  #           group = "Chinook Spawning Areas",
-  #           opacity = 0.5) %>%
-  addLegend(data = iptds_rec,        # consider moving to lower-left, if other legends are removed
+  addCircleMarkers(data = iptds_sf %>%
+                     filter(ptagis_active == TRUE),
+                   group = "Current IPTDS Sites",
+                   label = ~site_code,
+                   fillColor = ~om_col(integrated_om_site), # fill color
+                   fillOpacity = 0.9,                       # fill opacity
+                   color = "chartreuse",                    # border color
+                   opacity = 1,                             # border opacity
+                   weight = 1,                              # border thickness
+                   radius = 8,                              # marker size in pixels
+                   popup = paste("<b>Site Code:</b>", iptds_sf$site_code, "</br>",
+                                 "<b>Site Name:</b>", iptds_sf$site_name, "</br>",
+                                 "<b>Stream:</b>", iptds_sf$stream, "</br>",
+                                 "<b>PTAGIS Active:</b>", iptds_sf$ptagis_active, "</br>",
+                                 "<b>Arrays:</b>", iptds_sf$node_count, "</br>",
+                                 "<b>Antennas:</b>", iptds_sf$antenna_count, "</br>",
+                                 "<b>Biomark Integrated O&M Site:</b>", iptds_sf$integrated_om_site, "</br>",
+                                 "<b>Detection Probabilities:</b>", iptds_sf$detection_prob, "</br>",
+                                 # "<b>Current Funding:</b>", iptds_sf$current_funding, "</br>",
+                                 # "<b>BPA Funding Type:</b>", iptds_sf$bpa_funding, "</br>",
+                                 "<b>O&M Agency:</b>", iptds_sf$om_agency, "</br>",
+                                 "<b>O&M Responsibility:</b>", iptds_sf$om_responsibility, "</br>",
+                                 "<b>Site Description:</b>", iptds_sf$site_description, "</br>")) %>%
+  # add past iptds
+  addCircleMarkers(data = iptds_sf %>%
+                     filter(ptagis_active == FALSE),
+                   group = "Past IPTDS Sites",
+                   label = ~site_code,
+                   fillColor = ~om_col(integrated_om_site), # fill color
+                   fillOpacity = 0.9,                       # fill opacity
+                   color = "red",                           # border color
+                   opacity = 1,                             # border opacity
+                   weight = 1,                              # border thickness
+                   radius = 8,                              # marker size in pixels
+                   popup = paste("<b>Site Code:</b>", iptds_sf$site_code, "</br>",
+                                 "<b>Site Name:</b>", iptds_sf$site_name, "</br>",
+                                 "<b>Stream:</b>", iptds_sf$stream, "</br>",
+                                 "<b>PTAGIS Active:</b>", iptds_sf$ptagis_active, "</br>",
+                                 "<b>Arrays:</b>", iptds_sf$node_count, "</br>",
+                                 "<b>Antennas:</b>", iptds_sf$antenna_count, "</br>",
+                                 "<b>Biomark Integrated O&M Site:</b>", iptds_sf$integrated_om_site, "</br>",
+                                 "<b>Detection Probabilities:</b>", iptds_sf$detection_prob, "</br>",
+                                 # "<b>Current Funding:</b>", iptds_sf$current_funding, "</br>",
+                                 # "<b>BPA Funding Type:</b>", iptds_sf$bpa_funding, "</br>",
+                                 "<b>O&M Agency:</b>", iptds_sf$om_agency, "</br>",
+                                 "<b>O&M Responsibility:</b>", iptds_sf$om_responsibility, "</br>",
+                                 "<b>Site Description:</b>", iptds_sf$site_description, "</br>")) %>%
+  # add a legend for integrated o&m colors
+  addLegend(data = iptds_sf,
             position = "bottomleft",
-            pal = rec_col,
+            pal = om_col,
+            values = ~factor(integrated_om_site, levels = c("TRUE", "FALSE")),
+            title = "Integrated O&M Site",
+            group = "Current IPTDS Sites",
+            opacity = 0.8) %>%
+  # add iptds recommendations
+  addCircleMarkers(data = recommendations_sf,
+                   group = "IPTDS Recommendations",
+                   label = ~site_code,
+                   fillColor = ~recommendation_col(recommendation),
+                   fillOpacity = 0.8,
+                   color = ~priority_col(action_priority),
+                   opacity = 0.8,
+                   weight = 2,
+                   radius = 8,
+                   popup = paste("<b>Site Code:</b>", recommendations_sf$site_code, "</br>",
+                                 "<b>Priority:</b>", recommendations_sf$action_priority, "</br>",
+                                 "<b>Notes:</b>", recommendations_sf$notes, "</br>")) %>%
+  # add a legend for action priorities
+  addLegend(data = recommendations_sf,
+            position = "bottomleft",
+            pal = priority_col,
+            values = ~factor(action_priority, levels = c("HIGH", 
+                                                         "MED", 
+                                                         "LOW")),
+            title = "Priority (Border)",
+            group = "IPTDS Recommendations",
+            opacity = 0.8) %>%
+  # add a legend for recommendation colors
+  addLegend(data = recommendations_sf,
+            position = "bottomleft",
+            pal = recommendation_col,
             values = ~factor(recommendation, levels = c("Continue Funding", 
                                                         "Proposed New Site", 
                                                         "Candidate for O&M Project", 
@@ -213,22 +228,29 @@ sr_iptds_leaflet = base %>%
             title = "IPTDS Recommendations",
             group = "IPTDS Recommendations",
             opacity = 0.8) %>%
-  addLegend(data = iptds_cur,
-            position = "bottomleft",
-            pal = cur_col,
-            values = ~factor(integrated_om_site, levels = c("TRUE", "FALSE")),
-            title = "Integrated O&M Site",
-            group = "Current IPTDS Sites",
-            opacity = 0.8) %>%
-  addLegend(data = iptds_rec, 
-            position = "bottomright",
-            pal = priority_col,
-            values = ~factor(action_priority, levels = c("HIGH", "MED", "LOW")),
-            title = "Action Priority",
-            group = "Action Priority",
-            opacity = 0.5) %>%
-  hideGroup("DABOM MRR Sites") %>%
-  hideGroup("Action Priority")
+  # dabom mrr sites
+  addCircles(data = mrr_sites,
+             group = "DABOM MRR Sites",
+             label = ~site_code,
+             color = "magenta",
+             opacity = 1,
+             weight = 5,
+             popup = paste("<b>Site Code:</b>", mrr_sites$site_code, "</br>",
+                           "<b>Site Name:</b>", mrr_sites$site_name, "</br>",
+                           "<b>DABOM Node:</b>", mrr_sites$node, "</br>")) %>%
+  # control layers
+  addLayersControl(baseGroups = c("Steelhead Populations",
+                                  "Steelhead Spawning Areas",
+                                  "Sp/Sum Chinook Populations",
+                                  "Sp/Sum Chinook Spawning Areas"),
+                   overlayGroups = c("Current IPTDS Sites",
+                                     "Past IPTDS Sites",
+                                     "IPTDS Recommendations",
+                                     "DABOM MRR Sites"),
+                   options = layersControlOptions(collapsed = FALSE)) %>%
+  hideGroup("Past IPTDS Sites") %>%
+  hideGroup("IPTDS Recommendations") %>%
+  hideGroup("DABOM MRR Sites")
 
 sr_iptds_leaflet
 
