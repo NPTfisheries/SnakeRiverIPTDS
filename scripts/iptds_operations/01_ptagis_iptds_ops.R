68166848e002022dfd215826538826472938de5f
Mike Ackerman <dirtpathwanderer@gmail.com>
2026-01-09 13:28:22 -0700

    updated ptagis iptds operational dates for sy25

--- scripts/iptds_operations/01_ptagis_iptds_ops.R
+++ scripts/iptds_operations/01_ptagis_iptds_ops.R
@@ -4,7 +4,7 @@
 #   based on info from PTAGIS.
 # 
 # Created: April 29, 2024
-#   Last Modified: April 18, 2025
+#   Last Modified: January 9, 2026
 # 
 # Notes: 
 
@@ -18,7 +18,7 @@ library(janitor)
 library(here)
 
 # load configuration files
-load("C:/Git/SnakeRiverFishStatus/data/configuration_files/site_config_LGR_20250416.rda") ; rm(configuration, flowlines, crb_sites_sf, parent_child)
+load("C:/Git/SnakeRiverFishStatus/data/configuration_files/site_config_LGR_20260109.rda") ; rm(configuration, flowlines, crb_sites_sf, parent_child)
 
 # list of snake river interrogation sites
 sr_int_sites = sr_site_pops %>%
@@ -62,13 +62,13 @@ iptds_op_dates = sr_iptds_meta %>%
   mutate(first_date = as.Date(first_date),
          last_date = as.Date(last_date)) %>%
   # these sites don't have first_date; grabbed from PTAGIS
-  mutate(first_date = if_else(site_code == "BED", as.Date("2024-02-15"), first_date)) %>%
-  mutate(first_date = if_else(site_code == "CC4", as.Date("2024-08-06"), first_date)) %>%
   mutate(first_date = if_else(site_code == "CC5", as.Date("2024-12-16"), first_date)) %>%
-  mutate(first_date = if_else(site_code == "MDC", as.Date("2024-09-17"), first_date)) %>%
-  mutate(first_date = if_else(site_code == "UG3", as.Date("2024-05-23"), first_date)) %>%
-  mutate(first_date = if_else(site_code == "UG4", as.Date("2024-05-23"), first_date)) %>%
-  mutate(first_date = if_else(site_code == "COU", as.Date("2024-02-28"), first_date))
+  mutate(first_date = if_else(site_code == "EVM", as.Date("2024-11-16"), first_date)) %>%
+  mutate(first_date = if_else(site_code == "WH1", as.Date("2025-01-22"), first_date)) %>%
+  mutate(first_date = if_else(site_code == "WR3", as.Date("2025-07-16"), first_date)) %>%
+  mutate(first_date = if_else(site_code == "YF5", as.Date("2025-08-08"), first_date)) %>%
+  mutate(first_date = if_else(site_code == "YFW", as.Date("2025-08-08"), first_date))
+  
 
 # sequence of years that each site was in operation
 site_yrs = iptds_op_dates %>%
